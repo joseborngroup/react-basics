6da3b9029b003ff68ff16031a42b46c41512d390
Jose Paitamala <jose.paitamala@borngroup.com>
2019-12-26 16:46:56 -0300

    Updates

---
+++ precache-manifest.e75e1918327b184de81d2a5d165267bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94714b290d7afad26937e11ff0524c87",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f57304523679b856a368",
+    "url": "/monsters-rolodex/static/css/main.4d6eb315.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f57304523679b856a368",
+    "url": "/monsters-rolodex/static/js/main.afe5ff48.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
