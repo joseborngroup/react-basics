42fdb1ecbae1afb15239892b3adec2273114007e
Jose Paitamala <jose.paitamala@borngroup.com>
2019-12-26 08:49:26 -0300

    fetching content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            monsters: [
-                {
-                    name: 'Frankenstein',
-                    id: 'adad'
-                },
-                {
-                    name: ' Dracula',
-                    id: 'adsa'
-                },
-                {
-                    name: 'Zombie',
-                    id: 'alef'
-                }
-            ]
+            monsters: []
         };
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then( response => response.json())
+            .then( users => this.setState({ monsters: users }));
     }
     render() {
         return (
